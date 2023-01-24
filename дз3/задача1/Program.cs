1a09cc927131b9bc8ca4b480e690b405c717ba17
Ivan <v_dremov@list.ru>
2023-01-24 14:50:12 +0400

    Закончил

--- дз3/задача1/Program.cs
+++ дз3/задача1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5){
+    if (number[0] == number[4] && number[1] == number[3]) {
+        Console.WriteLine("число палиндром");
+    } else {
+        Console.WriteLine("число не палиндром");
+    }
+} else {
+    Console.WriteLine("Число не пятизначное");
+}
