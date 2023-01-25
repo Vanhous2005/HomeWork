4b829da0d8c52bb50c72a3bd807a24c0401cf3f6
Ivan <v_dremov@list.ru>
2023-01-25 16:21:47 +0400

    Закончил

---
+++ дз4/задача3/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] CreatArray() {
+    int [] array = new int [8];
+    Random number = new Random();
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = number.Next(0,1000);
+    }
+    return array;
+}
+int [] a = CreatArray();
+
+void PrintArray(int[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(a);
