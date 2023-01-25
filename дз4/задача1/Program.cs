41e2cc69aa31d48c00a133f70c4b04ad9770fcb8
Ivan <v_dremov@list.ru>
2023-01-25 15:37:35 +0400

    Закончил

---
+++ дз4/задача1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+
+while(true) {
+    Console.WriteLine("Для завершения программы введите -1 ");
+    string numberStr = Console.ReadLine();
+    int number = Convert.ToInt32(numberStr);
+
+    if(number == -1) {
+        break;
+    }
+  
+    string numberStr2 = Console.ReadLine();
+    int number2 = Convert.ToInt32(numberStr2);
+
+    Console.WriteLine(Math.Pow(number,number2));
+}
+
