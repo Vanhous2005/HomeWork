4f610f5f402550c25bf6f522a423c45b7c91c8a7
Ivan <v_dremov@list.ru>
2023-01-25 16:17:09 +0400

    Закончил

---
+++ дз4/задача2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Console.WriteLine("Напишите число");
+// string number = Console.ReadLine();
+// int num = Convert.ToInt32(number);
+// int sum = 0;
+// int index = 0;
+
+// while (num > 0) {
+//     num /= 10;
+//     index++;
+// }
+
+// for (int i = 0; i < index; i++) {
+//     sum = sum + num % 10;
+//     num = num / 10;
+// }
+// Console.WriteLine(sum);
+
+
+Console.WriteLine("Напишите число");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+int sum = 0;
+
+while (num != 0) {
+    sum = sum + num % 10;
+    num = num / 10;
+}
+Console.WriteLine(sum);
