5a764becbfadb932e0b9a15d048e7a4a310c89b9
Vanhous2005 <v_dremov@list.ru>
2023-01-30 17:55:42 +0400

    Закончил

--- дз5/задача3/Program.cs
+++ дз5/задача3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] CreatArray() {
+    int [] array = new int [4];
+    Random number = new Random();
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = number.Next(0,30);
+    }
+    return array;
+}
+int [] a = CreatArray();
+int min = 31;
+int max = 0;
+
+void PrintArray(int[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(a);
+
+for (int i = 0; i < a.Length; i++) {
+    if(min > a[i]) {
+        min = a[i];
+    }if (max < a[i]) {
+        max = a[i];
+    }
+}
+Console.WriteLine($"--> {max - min}");
