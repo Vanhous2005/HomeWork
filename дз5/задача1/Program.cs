22c953e327f117c26520eb78fae222b64b13cfcb
Vanhous2005 <v_dremov@list.ru>
2023-01-30 17:33:34 +0400

    Закончил

---
+++ дз5/задача1/Program.cs
@@ -0,0 +1,24 @@
+﻿int [] CreatArray() {
+    int [] array = new int [4];
+    Random number = new Random();
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = number.Next(100,1000);
+    }
+    return array;
+}
+int [] a = CreatArray();
+int count = 0;
+
+void PrintArray(int[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(a);
+
+for (int i = 0; i < a.Length; i++) {
+    if(a[i] % 2 == 0) {
+        count ++;
+    }
+}
+Console.WriteLine($"--> {count}");
