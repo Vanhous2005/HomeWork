eb073f8e573bb63469a723e8d7582fb2c0aa319d
Vanhous2005 <v_dremov@list.ru>
2023-01-30 17:42:37 +0400

    Закончил

--- дз5/задача2/Program.cs
+++ дз5/задача2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] CreatArray() {
+    int [] array = new int [4];
+    Random number = new Random();
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = number.Next(0,20);
+    }
+    return array;
+}
+int [] a = CreatArray();
+int sum = 0;
+
+void PrintArray(int[] array) {
+    for (int i = 0; i < array.Length; i++) {
+        Console.Write($"{array[i]} ");
+    }
+}
+PrintArray(a);
+
+for (int i = 0; i < a.Length; i++) {
+    if(i % 2 == 1) {
+        sum = sum + a[i];
+    }
+}
+Console.WriteLine($"--> {sum}");
